123b25da44a89232abc78b4551aacf06414164f6
Paulo Alberto Sousa <p.sousa612@gmail.com>
2022-05-08 02:54:21 -0700

    mongodb startup

---
+++ index.js
@@ -0,0 +1,45 @@
+const MongoClient = require('mongodb').MongoClient;
+let fs = require('fs');
+
+const url = 'mongodb://127.0.0.1:27017';
+const dbName = 'images';
+let db;
+
+const encodeImage = (img) => {
+  var bitmap = fs.readFileSync(img);
+  return new Buffer.from(bitmap).toString('base64');
+};
+
+MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
+  if (err) return console.log(err);
+
+  // Storing a reference to the database so you can use it later
+  db = client.db(dbName);
+  collectionName = 'main';
+
+  console.log(`Connected MongoDB: ${url}`);
+  console.log(`Database: ${dbName}`);
+
+  const imageName = 'udon.jpg';
+  const imgEncoded = encodeImage(`./${imageName}`);
+
+  const imageObj = {
+    name: imageName,
+    encodedImage: imgEncoded,
+  };
+
+  db.collection(collectionName).insertOne(imageObj, (err, res) => {
+    if (err) throw err;
+
+    console.log('Image inserted');
+  });
+});
+
+// db.collection('main')
+//   .find()
+//   .toArray((err, res) => {
+//     if (err) throw err;
+
+//     console.log(res);
+//     // db.close();
+//   });
